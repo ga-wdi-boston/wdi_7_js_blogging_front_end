41ea187a868fd29867cb0d6f53a6c5eb1107c7a1
J-Weeks <weeksjasons@gmail.com>
2015-02-27 17:24:28 -0500

    it works

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,7 @@
+// uniqueness contraint : must have a different email address
+//bob below
+// token 6014463b83fa4fefa80ea5ff0073557d
+
 /*global $:false*/
 'use strict';
 
@@ -9,6 +13,63 @@ var trace = function(){
 
 var App = App || {};
 
+App.submitUser = function(event, form){
+  if(event.preventDefault) event.preventDefault();
+  $.ajax({
+    url: 'http://localhost:3000/users',
+    type: 'POST',
+    dataType: 'JSON',
+    data: {
+      user:{
+        username: $('#username').val(),
+        email: $('#email').val(),
+        password: $('#password').val(),
+        password_confirmation: $('#passwordconfirmation').val(),
+        role: $('#role').val(),
+        first_name: $('#firstname').val(),
+        last_name: $('#lastname').val(),
+      }
+
+    },
+    sucess: function(data, textStatus, jqXHR){
+      trace('I name a new user', data, textStatus, jqXHR);
+    },
+  }).done(function(data){
+    trace(data);
+  }).fail(function(jqXHR, textStatus, errorThrown){
+    trace(jqXHR, textStatus, errorThrown);
+  });
+};
+
+App.submitPost = function(event){
+  if(event.preventDefault) event.preventDefault();
+  $.ajax({
+    url: 'http://localhost:3000/posts',
+    type: 'POST',
+    dataType: 'JSON',
+    data: {
+      post: {
+        title: $('#post-title').val(),
+        body: $('#post-body').val()
+      }
+    },
+    headers: { 'AUTHORIZATION': '6014463b83fa4fefa80ea5ff0073557d' },
+  }).done(function(data){
+    trace(data);
+  }).fail(function(jqXHR, textStatus, errorThrown){
+    trace(jqXHR, textStatus, errorThrown);
+  });
+  return false;
+};
+
 $(document).ready(function(){
+  var $userForm = $('form#user-form');
+  $userForm.on('submit', function(event){
+    App.submitUser(event,$userForm);
+  });
+  var $postForm = $('form#new-post-form');
+  $postForm.on('submit', function(event){
+    App.submitPost(event);
+  });
   trace('hello world');
 });
