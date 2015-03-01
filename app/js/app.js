331e58496566daf8dc5771ec4a54071cb2a19e2f
idelairre <idelairre@gmail.com>
2015-02-28 21:42:48 -0500

    got the categories footer to display and nothing else, weirdly.

--- app/js/app.js
+++ app/js/app.js
@@ -11,14 +11,15 @@ var display = function(data){
   var post = '';
   var postCategories = '';
   for(var i = 0; i < data.length; i++){
-    post += '<div id=' + i + '><p><b>' + data[i].title + '</b></p>' + '<p>' + data[i].body + '</p></div>';
     if(data[i].categories.length != 0){
-      for (var categoryIndex = 0; categoryIndex < data[i].categories; categoryIndex++){
+      for(var categoryIndex = 0; categoryIndex < data[i].categories.length; categoryIndex++){
+        post += '<div id=' + i + '><p><b>' + data[i].title + '</b></p>' + '<p>' + data[i].body + '</p></div>';
         postCategories += '<footer>' + data[i].categories[categoryIndex].id + " " + data[i].categories[categoryIndex].name + " " + data[i].categories[categoryIndex].created_at + " " + data[i].categories[categoryIndex].updated_at + '</footer>';
-    }
+      };
       $('#posts').html(post);
       $('#posts').html(postCategories);
-    } else {
+  } else {
+      post += '<div id=' + i + '><p><b>' + data[i].title + '</b></p>' + '<p>' + data[i].body + '</p></div>';
       $('#posts').html(post);
     };
   };
