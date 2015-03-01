caf1791847bf1e76433ea507e0771d2c1b6a18ab
pat <patrick.h.needham@gmail.com>
2015-03-01 14:08:41 -0500

    can show posts

--- app/js/app.js
+++ app/js/app.js
@@ -9,6 +9,88 @@ var trace = function(){
 
 var App = App || {};
 
+App.submitUser = function(event, form){
+  if (event.preventDefault) event.preventDefault();
+  $.ajax({
+    url: 'http://localhost:3000/users',
+    type: 'POST',
+    dataType: 'JSON',
+    data: {
+      user: {
+        username: $('#username').val(),
+        email: $('#email').val(),
+        password: $('#password').val(),
+        password_confirmation: $('#passwordconfirmation').val(),
+        role: $('#role').val(),
+        first_name: $('#firstname').val(),
+        last_name: $('lastname').val(),
+      }
+    },
+    success: function(data, textStatus, jqXHR){
+      trace(data, textStatus, jqXHR);
+    }
+  }).done(function(data){
+    trace(data);
+  }).fail(function(jqXHR, textStatus, errorThrown){
+    trace(jqXHR, textStatus, errorThrown);
+  });
+}
+
+App.submitPost = function(event){
+  if(event.preventDefault) event.preventDefault();
+  $.ajax({
+    url: 'http://localhost:3000/posts',
+    type: 'POST',
+    dataType: 'JSON',
+    data: {
+      post: {
+        title: $('#post-title').val(),
+        body: $('#post-body').val()
+      }
+    },
+    headers: { 'AUTHORIZATION': '3993a861c060430b82c24f9b6c8fd7d8' },
+  }).done(function(data){
+    trace(data);
+  }).fail(function(jqXHR, textStatus, errorThrown){
+    trace(jqXHR, textStatus, errorThrown);
+  });
+}
+
+App.showPosts = function(event){
+  $.ajax({
+    url: 'http://localhost:3000/posts',
+    type: 'GET',
+    dataType: 'JSON'
+  }).done(function(data){
+    trace('success');
+    trace(data);
+    for (var i = 0; i < data.length; i++) {
+      var post = "<div id='post-" + data[i].id + "'>";
+      post += "<h3>" + data[i].title + "</h3>";
+      post += "<p>" + data[i].body + "</p>";
+      post += "</div>";
+      $('.posts').append(post);
+    };
+  }).fail(function(jqXHR, textStatus, errorThrown){
+    trace(jqXHR, textStatus, errorThrown);
+  });
+}
+
 $(document).ready(function(){
-  trace('hello world');
+  var $userForm = $('form#user-form');
+  $userForm.on('submit', function(event){
+    App.submitUser(event, $userForm);
+  });
+
+  var $postForm = $('form#new-post-form');
+  $postForm.on('submit', function(event){
+    App.submitPost(event);
+  });
+
+  App.showPosts();
+
+  trace('if you can read this, you are probably a web developer');
 });
+
+// javy
+//3993a861c060430b82c24f9b6c8fd7d8
