815ca344317cfa595b9e960a800eb12970e0d4ed
J.Marx <jennyrutledge@gmail.com>
2015-03-01 10:57:14 -0500

    Create a new category. Reset form fields after posts.

--- app/js/app.js
+++ app/js/app.js
@@ -20,8 +20,15 @@ var App = (function(){
     setUsersData();
     var $userForm = $('form#user-form'); //children for form
   $userForm.on('submit',function(e){//e shorthand for event
-    submitUser(e,$userForm);
+   // submitUser(e,$userForm);
+   submitUser(e);
   });
+
+  var $categoryForm = $('form#category-form'); //children for form
+  $categoryForm.on('submit',function(e){//e shorthand for event
+    submitCategory(e);
+  });
+
   var $postForm = $('form#new-post-form');
   $postForm.on('submit', function(e){
 //debugger;
@@ -37,7 +44,6 @@ var App = (function(){
     usersData = input;
   };
 
-
   var getCategories = function(){
     $.ajax({
     url: 'http://localhost:3000/categories',
@@ -48,134 +54,158 @@ var App = (function(){
     showAllCategories(categoriesData);
     }).fail(function(jqXHR, textStatus, errorThrown){
     trace(jqXHR, textStatus, errorThrown);
-  });
-};
-
-var showAllCategories = function(categoriesData){
-    // for(var i = 0; i < categories.length; i++){
-    //   $('#categories').append('<li>' + categories[i].name +'</li>')
-    // };
-    for(var i = 0; i < categoriesData.length; i++){
-    $('#post-category').append('<li><input type="checkbox" name="categories" value="' + categoriesData[i].id+'"  id="' +categoriesData[i].id + '" />' + categoriesData[i].name + '</li>' );
-    };
+    });
   };
 
-var updateCategories = function(postID,categoryIdArr){
-  debugger;
-  categoryIdArr.forEach(function(categoryId){
+  var submitCategory = function(e){
+    if(e.preventDefault) e.preventDefault();
     $.ajax({
-    url: 'http://localhost:3000/posts/' + postID + '/categories/' + categoryId,
-    type: 'PATCH',
-    headers: {'AUTHORIZATION': '015b0bb0383046f5ae3d2ee213fd14cc'}
-    }).done(function(data){}).fail();
-  });
-};
-
-var getPostData = function(){
-  $.ajax({
-    url: 'http://localhost:3000/users',
-    type: 'GET',
-  }).done(function(usersData){
-   //trace(userData);
-   setUsersData(usersData);
-   App.showAllPosts(usersData);
-  }).fail(function(jqXHR, textStatus, errorThrown){
-    trace(jqXHR, textStatus, errorThrown);
-  });
-};
-
-var showAllPosts = function(usersData){
-  for(var i = 0; i < usersData.length; i++){
-     // debugger;
-      App.showUserPosts(usersData[i].posts)
-    };
-};
-
-var showUserPosts = function(postData){
- // debugger;
-  for(var i = 0; i < postData.length; i++){
-    $('#posts').append('<li>' + postData[i].body + '</li>' + postData[i].categoriesData) };
-};
-
-var showPostsCategories = function(postCategories){
- // debugger;
-  for(var i = 0; i < postCategories.length; i++){
-    $('#posts').append('<li>' + postCategories[i].body + '</li>' + postCategories[i]) };
-};
-
-var checkedPostCategories = function(){
-  var categoryIdArr = [];
-  var categoryList = $('#post-category').children();
-  for(var i = 0; i < categoryList.length; i++){
-    if($('#'+ $(categoryList[i]).children().val()).is(':checked')){
-     // categoryArr.push(categoriesData[i]);
-     categoryIdArr.push($(categoryList[i]).children().val());
-    };
-  };
-  debugger;
-  return categoryIdArr;
-};
-
-var submitUser = function(e, form){
-  if(e.preventDefault) e.preventDefault();
-  $.ajax({
-    url: 'http://localhost:3000/users',
-    type: 'POST',
-    dataType: 'JSON',
-    data: {
-      user:{
-        username:$('#username').val(),
-        email: $('#email').val(),
-        password: $('#password').val(),
-        password_confirmation: $('#passwordconfirmation').val(),
-        role: $('#role').val(),
-        first_name: $('#firstname').val(),
-        last_name: $('#lastname').val()
+      url: "http://localhost:3000/categories",
+      type: 'POST',
+      dataType: 'JSON',
+      data: {
+        category: {
+          name: $('#category-name').val(),
         }
-    },
-    success: function(data,textStatus, jqXHR){ //setting in my ajax object
-      trace('I made a new user!!!', data, textStatus, jqXHR);
-    }
-  }).done(function(data){
-    trace(data);
-  }).fail(function(jqXHR, textStatus, errorThrown){
-    trace(jqXHR, textStatus, errorThrown);
-  });
-};
-
-var submitPost = function(e){
-  if(event.preventDefault) event.preventDefault();
-  $.ajax({
-    url:"http://localhost:3000/posts",
-    type: 'POST',
-    dataType: 'JSON',
-    data: {
-      post: {
-        title: $('#post-title').val(),
-        body: $('#post-body').val()
-      }
-    },
-    headers: {'AUTHORIZATION': '015b0bb0383046f5ae3d2ee213fd14cc'}
-  }).done(function(postData){
-      updateCategories(postData.id,checkedPostCategories());;
-     // trace(data);
-  }).fail(function(jqXHR, textStatus, errorThrown){
-      trace(jqXHR, textStatus, errorThrown);
-  });
-};
-
-return{
-  init: init,
-  submitPost:submitPost,
-  submitUser:submitUser,
-  checkedPostCategories:checkedPostCategories,
-  showPostsCategories:showPostsCategories,
-  showUserPosts:showUserPosts,
-  showAllPosts:showAllPosts,
-  getData: getData,
-  showAllCategories:showAllCategories,
-  getCategories:getCategories
+      },
+      headers: {'AUTHORIZATION': '015b0bb0383046f5ae3d2ee213fd14cc'}
+    }).done(function(categoryData){
+      $('#category-name').val('');
+      trace(categoryData);
+    }).fail(function(jqXHR, textStatus, errorThrown){
+        trace(jqXHR, textStatus, errorThrown);
+    });
   };
+
+  var showAllCategories = function(categoriesData){
+      // for(var i = 0; i < categories.length; i++){
+      //   $('#categories').append('<li>' + categories[i].name +'</li>')
+      // };
+      for(var i = 0; i < categoriesData.length; i++){
+      $('#post-category').append('<li><input type="checkbox" name="categories" value="' + categoriesData[i].id+'"  id="' +categoriesData[i].id + '" />' + categoriesData[i].name + '</li>' );
+      };
+    };
+
+  var updateCategories = function(postID,categoryIdArr){
+    debugger;
+    categoryIdArr.forEach(function(categoryId){
+      $.ajax({
+      url: 'http://localhost:3000/posts/' + postID + '/categories/' + categoryId,
+      type: 'PATCH',
+      headers: {'AUTHORIZATION': '015b0bb0383046f5ae3d2ee213fd14cc'}
+      }).done(function(data){
+        trace(data);
+      }).fail();
+    });
+  };
+
+  var getPostData = function(){
+    $.ajax({
+      url: 'http://localhost:3000/users',
+      type: 'GET',
+    }).done(function(usersData){
+     //trace(userData);
+     setUsersData(usersData);
+     App.showAllPosts(usersData);
+    }).fail(function(jqXHR, textStatus, errorThrown){
+      trace(jqXHR, textStatus, errorThrown);
+    });
+  };
+
+  var showAllPosts = function(usersData){
+    for(var i = 0; i < usersData.length; i++){
+       // debugger;
+        App.showUserPosts(usersData[i].posts);
+      };
+  };
+
+  var showUserPosts = function(postData){
+   // debugger;
+    for(var i = 0; i < postData.length; i++){
+      $('#posts').append('<li>' + postData[i].body + '</li>' + postData[i].categoriesData) };
+  };
+
+  var showPostsCategories = function(postCategories){
+   // debugger;
+    for(var i = 0; i < postCategories.length; i++){
+      $('#posts').append('<li>' + postCategories[i].body + '</li>' + postCategories[i]) };
+  };
+
+  var submitPost = function(e){
+    if(e.preventDefault) e.preventDefault();
+    $.ajax({
+      url:'http://localhost:3000/posts',
+      type: 'POST',
+      dataType: 'JSON',
+      data: {
+        post: {
+          title: $('#post-title').val(),
+          body: $('#post-body').val()
+        }
+      },
+      headers: {'AUTHORIZATION': '015b0bb0383046f5ae3d2ee213fd14cc'}
+    }).done(function(postData){
+        updateCategories(postData.id,checkedPostCategories());
+        $('#post-title, #post-body').val('');
+       // trace(data);
+    }).fail(function(jqXHR, textStatus, errorThrown){
+        trace(jqXHR, textStatus, errorThrown);
+    });
+  };
+
+  var checkedPostCategories = function(){
+    var categoryIdArr = [];
+    var categoryList = $('#post-category').children();
+    for(var i = 0; i < categoryList.length; i++){
+      if($('#'+ $(categoryList[i]).children().val()).is(':checked')){
+       // categoryArr.push(categoriesData[i]);
+       categoryIdArr.push($(categoryList[i]).children().val());
+      };
+      $('#'+ $(categoryList[i]).children().val()).prop('checked', false);
+    };
+    return categoryIdArr;
+  };
+
+  var submitUser = function(e){
+    if(e.preventDefault) e.preventDefault();
+    $.ajax({
+      url: 'http://localhost:3000/users',
+      type: 'POST',
+      dataType: 'JSON',
+      data: {
+        user:{
+          username:$('#username').val(),
+          email: $('#email').val(),
+          password: $('#password').val(),
+          password_confirmation: $('#passwordconfirmation').val(),
+          role: $('#role').val(),
+          first_name: $('#firstname').val(),
+          last_name: $('#lastname').val()
+          }
+      },
+      success: function(data,textStatus, jqXHR){ //setting in my ajax object
+        trace('I made a new user!!!', data, textStatus, jqXHR);
+      }
+    }).done(function(data){
+       $('#username, #email,,#password,#passwordconfirmation,#role, #firstname,#lastname').val('');
+       trace(data);
+    }).fail(function(jqXHR, textStatus, errorThrown){
+      trace(jqXHR, textStatus, errorThrown);
+    });
+  };
+
+  return{
+    init: init,
+    submitPost:submitPost,
+    submitUser:submitUser,
+    checkedPostCategories:checkedPostCategories,
+    showPostsCategories:showPostsCategories,
+    showUserPosts:showUserPosts,
+    showAllPosts:showAllPosts,
+    getPostData: getPostData,
+    showAllCategories:showAllCategories,
+    getCategories:getCategories
+    };
 })();
 
 $(document).ready(function(){
