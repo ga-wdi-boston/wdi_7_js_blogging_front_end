a933d8b95e6a8c9ef873ab7dcd930f966c2ee794
idelairre <idelairre@gmail.com>
2015-02-28 19:40:41 -0500

    carried over display button from a previous exercise. About to change it so that posts display on load.

--- app/js/app.js
+++ app/js/app.js
@@ -7,8 +7,98 @@ var trace = function(){
   }
 };
 
+var display = function(data){
+  var string = '';
+  for(var i = 0; i < data.length; i++){
+    string += '<p>' + data[i].title + '</p>' + '<p>' + data[i].body + '</p>';
+    $('#posts').html(string);
+  };
+};
+
 var App = App || {};
 
 $(document).ready(function(){
   trace('hello world');
 });
+
+// always write out the entire structure of your function before you write it out
+
+  App.submitUser = function(event, form){
+  if(event.preventDefault) event.preventDefault();
+    $.ajax({
+      url: 'http://localhost:3000/users',
+      type: 'POST',
+      dataType: 'JSON',
+      data: {
+        user: {
+        username: $('#username').val(),
+        email: $('#email').val(),
+        password: $('#password').val(),
+        password_confirmation: $('passwordconfirmation').val(),
+        role: $('#role').val(),
+        first_name: $('#firstname').val(),
+        last_name: $('#lastname').val()
+    }
+  },
+  success: function(data, textStatus, jqXHR){
+    trace('I made a new user!!!', data, textStatus, jqXHR);
+    },
+  }).done(function(data){
+    trace(data);
+  }).fail(function(jqXHR, textStatus, errorThrown){
+    trace(jqXHR, textStatus, errorThrown);
+  });
+};
+
+App.submitPost = function(event){
+  if(event.preventDefault) event.preventDefault();
+  $.ajax({
+    url: 'http://localhost:3000/posts',
+    type: 'POST',
+    dataType: 'JSON',
+    data: {
+      post: {
+        title: $('#post-title').val(),
+        body: $('#post-body').val()
+      }
+    },
+    headers: { 'AUTHORIZATION': "109885dca2fc451fbca7d7795ff65355" },
+    }).done(function(data){
+      trace(data);
+    }).fail(function(jqXHR, textStatus, errorThrown){
+    });
+    return false;
+  };
+
+App.displayPosts = function(event){
+  // if(event.preventDefault) event.preventDefault();
+  $.ajax({
+    url: 'http://localhost:3000/posts',
+    type: 'GET'
+    }).done(function(data){
+      trace(data);
+      display(data);
+    }).fail(function(jqXHR, textStatus, errorThrown){
+    });
+    return false
+  };
+
+$('document').ready(function(){
+  var $userForm = $('form#user-form');
+  $userForm.on('submit', function(event){
+    App.submitUser(event, $userForm);
+  });
+
+  var $postForm = $('form#new-post-form');
+  $postForm.on('submit', function(event){
+    App.submitPost(event);
+  });
+
+  $('#display').click(App.displayPosts);
+
+  // var $posts = $('#posts');
+  // $posts.on('load', function(event){
+  //   App.displayPost(event);
+  // });
+});
+/*"109885dca2fc451fbca7d7795ff65355"*/
