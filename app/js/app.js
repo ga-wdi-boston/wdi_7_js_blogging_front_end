09cbdaf3d57aa9a70fa3007bbe236e435b655a9b
katyf <katy.feng.14@gmail.com>
2015-03-01 23:22:55 -0500

    in progress. Submit new users, posts, show posts

--- app/js/app.js
+++ app/js/app.js
@@ -7,8 +7,193 @@ var trace = function(){
   }
 };
 
-var App = App || {};
+var App = App || {
+  $posts: $('.posts')
+};
+
+App.submitUser = function(event, form){
+  if(event.preventDefault) event.preventDefault();
+  $.ajax({
+    url: 'http://localhost:3000/users',
+    type: 'POST',
+    dataType: 'json',
+    data: {
+      user: {
+        username: $('#username').val(),
+        email: $('#email').val(),
+        password: $('#password').val(),
+        password_confirmation: $('#passwordconfirmation').val(),
+        role: $('#role').val(),
+        first_name: $('#firstname').val(),
+        last_name: $('#lastname').val(),
+      }
+    },
+    success: function(data, textStatus, jqXHR){
+      console.log('New User created!', data, textStatus, jqXHR);
+        }
+  })
+  .done(function(data) {
+    trace(data);
+    $('#user-form .clearme').val('');
+  })
+  .fail(function(jqXHR, textStatus, errorThrown) {
+    console.log(jqXHR, textStatus, errorThrown);
+  });
+
+};
+
+App.submitPost = function(event){
+  if(event.preventDefault) event.preventDefault();
+  var $title = $('#post-title').val();
+  var $body = $('#post-body').val();
+
+  $.ajax({
+    url: 'http://localhost:3000/posts',
+    type: 'POST',
+    dataType: 'json',
+    data: {
+      post: {
+        title: $('#post-title').val(),
+        body: $('#post-body').val(),
+      }
+    },
+    headers: { 'AUTHORIZATION': 'c9436520a9ef4cd099b95bb9a41738f2' },
+  })
+  .done(function(data) {
+    trace(data);
+    $('#new-post-form .clearme').val('');
+  })
+  .fail(function(jqXHR, textStatus, errorThrown) {
+    console.log(jqXHR, textStatus, errorThrown);
+  });
+
+   $.ajax({
+    url: 'http://localhost:3000/posts',
+    type: 'GET',
+  })
+  .done(function(results) {
+    console.log("success");
+    results.forEach(App.showPosts);
+  });
+};
+
+App.showOnePost = function(post){
+  $.ajax({
+    url: 'http://localhost:3000/posts/' + post.id,
+    type: 'GET',
+    data: {
+      post: {
+        title: $('#post-title'),
+        body: $('#post-body'),
+      }
+    },
+  })
+  .done(function() {
+    console.log("success");
+  })
+  .fail(function() {
+    console.log("error");
+  });
+};
+
+
+App.showPosts = function(post) {
+  var div = $('<div class="eachpost">');
+  var  html = $('<h3>' + post.title + '</h3>');
+  var postBody = $('<p class="pbody">' + post.body + '</p><br>');
+  var button = $('<br><button class="catbtn">Add a Category</button>');
+  var deleteBtn = $('<button class="delbtn">Delete Post</button>');
+  postBody.append(button);
+  postBody.append(deleteBtn);
+  html.append(postBody);
+  div.append(html);
+  $('.posts').append(div);
+};
+
+App.showAllPosts = function(){
+  $.ajax({
+    url: 'http://localhost:3000/posts',
+    type: 'GET',
+  })
+  .done(function(results) {
+    console.log("success");
+    results.forEach(App.showPosts);
+    $('.pbody').hide();
+    $('.eachpost').on('mouseenter', function() {
+    //console.log('click works');
+    $(this).closest('.eachpost').find($('.pbody')).slideDown();
+    $(this).addClass('highlighted');
+  });
+    $('.eachpost').on('mouseleave', function() {
+    //console.log('click works');
+    $(this).closest('.eachpost').find($('.pbody')).slideUp();
+    $(this).removeClass('highlighted');
+  });
+    $('.delbtn').on('click', function() {
+        $(this).deletePost();
+    });
+  });
+};
+
+// App.addCategory = function(event){
+//   if(event.preventDefault) event.preventDefault();
+//   $.ajax({
+//     url: 'http://localhost:3000/posts/' + post.id + '/categories/' + category.id,
+//     type: 'PATCH',
+//     data: {
+//       post: {
+//         categories: $('#post-category').val()
+//       }
+//     }
+//   })
+//   .done(function(results) {
+//     console.log('category added');
+//   });
+// };
+App.deletePost = function(post){
+  $.ajax({
+    url: 'http://localhost:3000/posts' + post.id,
+    type: 'DELETE',
+  })
+  .done(function() {
+    console.log("post deleted");
+  })
+  .fail(function() {
+    console.log("error");
+  });
+
+};
 
 $(document).ready(function(){
   trace('hello world');
+  $('#new-post-form').hide();
+  $('#user-form').hide();
+
+  $('#post-btn').on('click', function() {
+    $('#new-post-form').slideToggle();
+  });
+  $('#user-btn').on('click', function() {
+    $('#user-form').slideToggle();
+  });
+  App.showAllPosts();
+   $('h1').on('click', function() {
+    console.log('click works');
+    $('.togtest').toggle();
+  });
+
+  var $userForm = $('form#user-form');
+  $userForm.on('submit', function(event){
+    App.submitUser(event, $userForm);
+  });
+
+  var $postForm = $('form#new-post-form');
+  $postForm.on('submit', function(event){
+    App.submitPost(event);
+    App.showPosts(event);
+  });
 });
+
+
+
+// merp token = 369d401a3b71418c84be93f0d3e5caa9
+// katyf token = c9436520a9ef4cd099b95bb9a41738f2
