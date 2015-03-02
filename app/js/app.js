41289dca60b6077243020b44b92245c6fdb0b59f
Doug <djmacdonald@uchicago.edu>
2015-03-02 09:15:29 -0500

    a

--- app/js/app.js
+++ app/js/app.js
@@ -10,5 +10,70 @@ var trace = function(){
 var App = App || {};
 
 $(document).ready(function(){
-  trace('hello world');
+  var $userForm = $('form#user-form');
+  $userForm.on('submit', function(event){
+    App.submitUser(event, $userForm);
+  });
+  var $postForm = $('form#new-post-form');
+  $postForm.on('submit', function(event){
+    App.submitPost(event, $postForm);
+  });
 });
+
+App.submitUser = function(event, form){
+  if(event.preventDefault) {event.preventDefault();}
+  $.ajax({
+    url: 'http://localhost:3000/users',
+    type: 'POST',
+    dataType: 'JSON',
+    data: {
+      user: {
+        username: $('#username').val(),
+        email: $('#email').val(),
+        password: $('#password').val(),
+        password_confirmation: $('#passwordconfirmation').val(),
+        role: $('#role').val(),
+        first_name: $('#firstname').val(),
+        last_name: $('#lastname').val()
+      }
+    },
+    success: function(data, textStatus, jqXHR){
+      trace('I made a new user!!!', data, textStatus, jqXHR);
+    },
+  }).done(function(data){
+    trace(data);
+  }).fail(function(jqXHR, textStatus, errorThrown){
+    trace(jqXHR, textStatus, errorThrown);
+  });
+};
+
+App.submitPost = function(event, form){
+  if(event.preventDefault) {event.preventDefault();}
+  $.ajax({
+    url: 'http://localhost:3000/posts',
+    type: 'POST',
+    dataType: 'JSON',
+    data: {
+      post: {
+        title: $('#post-title').val(),
+        body: $('#post-body').val()
+      }
+    },
+    headers: {'AUTHORIZATION': 'e4fdfb2b2e674ede93cc8204d38c0581'},
+    success: function(data, textStatus, jqXHR){
+      trace('I made a new post!!', data, textStatus, jqXHR);
+    },
+  }).done(function(data){
+    trace(data);
+  }).fail(function(jqXHR, textStatus, errorThrown){
+    trace(jqXHR, textStatus, errorThrown);
+  });
+  return false;
+};
+
+
+
+
+// username: doug
+// password: 1
+// e4fdfb2b2e674ede93cc8204d38c0581
