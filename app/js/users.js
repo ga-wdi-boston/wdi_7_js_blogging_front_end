5c1aef1958cdba4ca4038ea07b47516801ea7ab9
Dan Stoppelman <dstop75@gmail.com>
2015-03-01 19:42:42 -0500

    add editUser function but don't display

--- app/js/users.js
+++ app/js/users.js
@@ -2,9 +2,9 @@
 'use strict';
 
 var trace = function(){
-  for(var i = 0; i < arguments.length; i++){
-    console.log(arguments[i]);
-  }
+    for(var i = 0; i < arguments.length; i++){
+        console.log(arguments[i]);
+    }
 };
 
 var App = App || {};
@@ -13,26 +13,24 @@ App.ajaxFail = function(jqXHR, textStatus, errorThrown) {
     trace(jqXHR, textStatus, errorThrown);
 };
 
-var postNewUser = {
-    url: 'http://localhost:3000/users',
-    type: 'POST',
-    dataType: 'JSON',
-    data: {
-        user: {
-            username: $('#username').val(),
-            email: $('#email').val(),
-            password: $('#password').val(),
-            password_confirmation: $('#passwordconfirmation').val(),
-            role: $('#role').val(),
-            first_name: $('#firstname').val(),
-            last_name: $('#lastname').val()
-        }
-    }
-};
-
 App.submitUser = function(event) {
     event.preventDefault();
-    $.ajax(postNewUser).done(function(data, textStatus, jqXHR) {
+    $.ajax({
+        url: 'http://localhost:3000/users',
+        type: 'POST',
+        dataType: 'JSON',
+        data: {
+            user: {
+                username: $('#username').val(),
+                email: $('#email').val(),
+                password: $('#password').val(),
+                password_confirmation: $('#passwordconfirmation').val(),
+                role: $('#role').val(),
+                first_name: $('#firstname').val(),
+                last_name: $('#lastname').val()
+            }
+        }
+    }).done(function(data, textStatus, jqXHR) {
         trace('I made a new user!!!!', data, textStatus, jqXHR);
         App.getUsers();
     }).fail(App.ajaxFail);
@@ -51,20 +49,42 @@ App.renderUsers = function(users) {
     users.forEach(App.createUserHTML);
 };
 
-App.getUsers = function() {
-    $.get('http://localhost:3000/users', App.renderUsers, 'json')
-    .fail(App.ajaxFail);
-};
-
 App.renderUser = function(user) {
     // add code to create HTML and display user
     trace('Success!', user);
+};
+
+App.getUsers = function() {
+    $.get('http://localhost:3000/users', App.renderUsers, 'json')
+    .fail(App.ajaxFail);
 };
 
 App.getUser = function(event) {
     event.preventDefault();
     $.get('http://localhost:3000/users/' + this.id, App.renderUser, 'json')
     .fail(App.ajaxFail);
+};
+
+App.editUser = function(id) {
+    $.ajax({
+        url: 'http://localhost:3000/users/' + id,
+        type: 'patch',
+        dataType: 'json',
+        data: {
+            user: {
+                username: $('#username').val(),
+                email: $('#email').val(),
+                password: $('#password').val(),
+                password_confirmation: $('#passwordconfirmation').val(),
+                role: $('#role').val(),
+                first_name: $('#firstname').val(),
+                last_name: $('#lastname').val()
+            }
+        }
+    }).done(function(data, textStatus, jqXHR) {
+        trace('I edited a user!!!!', data, textStatus, jqXHR);
+        App.getUsers();
+    }).fail(App.ajaxFail);
 };
 
 $(document).ready(function() {
